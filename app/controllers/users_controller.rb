a923db75e1197fcc407a767ccb94e5431fa423b4
Galiwango Ananiya <galiwango101@gmail.com>
2021-01-25 02:02:34 +0300

    update create user action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,12 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    
+    if @user.save
+      redirect_to users_new_path
+    else
+      render :new
+    end
   end
 end
