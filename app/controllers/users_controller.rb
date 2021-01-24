8f13769a52e3865021b6da7af30917d1a71f0355
Galiwango Ananiya <galiwango101@gmail.com>
2021-01-25 02:34:54 +0300

    update user controller action and the new view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,9 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
-    
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    @user = User.new(user_params)
+
     if @user.save
       redirect_to users_new_path
     else
