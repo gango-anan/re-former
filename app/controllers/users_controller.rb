41ca461c968949b89b84ed0d2fdaacecba624548
Galiwango Ananiya <galiwango101@gmail.com>
2021-01-25 23:06:45 +0300

    add edit view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,16 +6,24 @@ class UsersController < ApplicationController
   def create
     # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
     @user = User.new(user_params)
-
     if @user.save
-      redirect_to users_new_path
+      redirect_to new_user_path
     else
       render :new
     end
   end
 
   def edit
+    @user = User.find(params[:id])
   end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to edit_user_path
+    else
+      render :edit
+    end
   end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # resources :users, only: [:new, :create]
-  get 'users/new'
-  post 'users/create'
+  resources :users, only: [:new, :create, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
