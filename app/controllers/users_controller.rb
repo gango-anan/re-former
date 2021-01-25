5f0c641cf5f03d53331945e01b518b646664f253
Galiwango Ananiya <galiwango101@gmail.com>
2021-01-25 23:50:44 +0300

    fix errors flagged by Rubocop

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
-  
+
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
@@ -28,7 +28,7 @@ class UsersController < ApplicationController
 end
 
 private
-    
+
 def user_params
-  params.require(:user).permit(:username,:email,:password)
+  params.require(:user).permit(:username, :email, :password)
 end
